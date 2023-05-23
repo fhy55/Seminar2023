8b0ba217f2703fadc2393b60f82955a41afc3b2b
fhy55 <rbcnfukihara55@gmail.com>
2023-05-23 17:17:37 +0900

    chapter 03 practice set

--- code/chapter03.R
+++ code/chapter03.R
@@ -1,3 +1,65 @@
+#practice set
+ice_path <- here::here("data/data/data/chap03/icecream.csv")
+icedata <- read.csv(ice_path)
+
+tmp_path <- here::here("data/data/data/chap03/temperature.csv")
+tmpdata <- read.csv(tmp_path)
+
+
+#3.2
+mean_all_tmp <- mean(tmpdata$temp)
+
+head_100_tmp <- head(tmpdata,100)
+mean_head_tmp <- mean(head_100_tmp$temp)
+
+#無作為抽出は復元？被復元？
+random_tmp <- sample(tmpdata,size=100,replace=TRUE)
+random_tmp
+mean_random <- mean(random_tmp$temp)
+#頭から抜きだした場合、日付順にソートされている為、日にちと温度の相関が存在し、似た温度が抽出される
+
+#3.3
+max_index <- which.max(icedata$icecream)
+icedata$city[max_index]
+
+plot(icedata$icecream,icedata$income)
+cor(icedata$icecream,icedata$income)
+
+
+plot(icedata$icecream,icedata$u15)
+cor(icedata$icecream,icedata$u15)
+#icecreamの消費とincomeの方が相関が強い。しかし、相関は因果と異なるため、交絡因子を確認する必要がある
+#子供の多い地方は、都市部でなく地方である。地方には、あまりアイスが流通していない可能性もある。(日本はそんなことないが)
+#3.4
+
+x <-rnorm(1000,50,10)
+y <-rnorm(1000,50,10)
+
+#10は-3σ以上だから、実現確率は1パーセント未満
+#P(X>10)=ΣI{x>10}P(x>10)
+mean(x>10)
+
+mean(x>-10)-mean(x>10)
+
+mean(x>y**2)
+
+#3.5
+
+pop_mean <-50
+s2 <- function(x){ var(x)*(length(x)-1)/length(x) }
+cnt <- 0
+S <-10000
+for(i in 1:S){
+  n<-1000
+  x <-rnorm(n,50,10)
+  bar_x <-mean(x)
+  sample_var<- s2(x)
+  sample_deviation <- sqrt(sample_var)
+  CI_lower <-mean(x)-1.65*sample_deviation/sqrt(n)
+  CI_upper <- mean(x)+1.65*sample_deviation/sqrt(n)
+  if(CI_upper>50 && CI_lower<50){cnt <- cnt+1}
+}
+cnt
 
 
 set.seed(2022)
@@ -54,6 +116,8 @@ mean(X > 5) * mean(Z > 10)
 
 # Pr(X > 5 かつ Z > 10)
 mean((X > 5) * (Z > 10))
+
+1.41**2+(-0.82)**2+0.58**2
 
 
 X <- rnorm(100000, 50, 10)
