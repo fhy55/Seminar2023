a8094521c0c17fe74fa66d6490ed770e743ec3aa
fhy55 <rbcnfukihara55@gmail.com>
2023-07-26 17:50:59 +0900

    add chapter06

--- code/chapter06.R
+++ code/chapter06.R
@@ -13,6 +13,9 @@ videodata %>%
   summarise(cor = cor(grade, hours))  
 crimedata <- readr::read_csv("police_crime.csv")
 
+crime_path <- here::here("data/data/data/chap06/police_crime.csv")
+crimedata <- readr::read_csv(crime_path)
+
 crimedata %>% 
   ggplot(aes(x = police, y = crime)) +
   geom_point()
@@ -25,12 +28,13 @@ set.seed(2022)
 n  <- 400
 D  <- rbinom(n, 1, 0.6)  # 確率 0.6 で 1，確率 0.4 で 0 をとるトリートメント変数
 TE <- 2                  # トリートメント効果 = 2 で個人間の差は無いと想定
+#トリートメントダミーの切片無回帰が、トリートメント効果
 Y  <- TE * D + rnorm(n)  # 結果変数
 
 EY1 <- sum(D * Y) / sum(D)
 EY0 <- sum((1 - D) * Y) / sum(1 - D)
 EY1 - EY0
-
+#重要:一様分布に従ってランダムにトリートメント変数D=0 or 1を割り当てる
 Z <- runif(n)        # D と Y に共通して影響を与える変数
 D <- rbinom(n, 1, Z) # Z が大きいほど 1 をとりやすいトリートメント変数
 Y <- TE * D + (2 * Z - 1) + rnorm(n)
